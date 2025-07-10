704ebf210776b4f985d9377111dadfab8c97735a
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-10 13:44:05 +0530

    Update .gitignore to exclude 'exception' directory; add DivisionDemo class for exception handling demonstration

---
+++ JavaTrainingPune/src/exception/DivisionDemo.java
@@ -0,0 +1,34 @@
+// Define the package name (this should match your folder structure)
+package exception;
+
+public class DivisionDemo {
+    public static void main(String[] args) {
+        try {
+            // Try to parse the first command line argument to an integer
+            int num1 = Integer.parseInt(args[0]);
+            System.out.println("First Number is : " + num1);
+            
+            // Try to parse the second command line argument to an integer
+            int num2 = Integer.parseInt(args[1]);
+            System.out.println("Second Number is : " + num2);	
+            
+            // Perform division and display the result
+            System.out.println("Division Result of " + num1 + " and " + num2 + " is : " + (num1 / num2));
+        } 
+        // This block handles the case where inputs are not valid integers
+        catch (NumberFormatException exception) {
+            System.out.println("Please pass proper Numeric Value !!! " + exception.getMessage());
+        } 
+        // This block handles the case where fewer than two arguments are passed
+        catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Pass 2 command line arguments !!! " + exception.getMessage());
+        } 
+        // This block handles division by zero errors
+        catch (ArithmeticException exception) {
+            System.out.println("Division by zero NOT Possible !!! " + exception.getMessage());
+        }
+
+        // This statement runs regardless of whether an exception occurs or not
+        System.out.println("End of the application");
+    }
+}
--- JavaTrainingPune/src/test/ObjectClassDemo.java
+++ JavaTrainingPune/src/test/ObjectClassDemo.java
@@ -2,21 +2,39 @@ package test;
 
 public class ObjectClassDemo {
     public static void main(String[] args) {
-        Employee employee=new Employee(101, "Neha", 150000.00f);
-        System.out.println("HashCode of the employee : "+ employee.hashCode());
-        System.out.println("\nemployee "+employee);
-        System.out.println("\nemployee : " +employee.toString());
+        // Creating an Employee object
+        Employee employee = new Employee(101, "Neha", 150000.00f);
 
-        Employee employee2=new Employee(101, "Neha", 150000.00f);
-        System.out.println("\nemployee "+employee2);
-        System.out.println("\nemployee : " +employee2.toString());
-        //System.out.println(employee==employee2);
-        System.out.println("\n employee.equals(employee2) :"+employee.equals(employee2));//false
-        
-        
-        
-        Employee employee3=employee.clone();
-        System.out.println("Clone of employee : employee3 : "+employee3);
+        // Displaying the hash code of the employee object
+        System.out.println("HashCode of the employee : " + employee.hashCode());
 
+        // Printing the employee object (implicitly calls toString())
+        System.out.println("\nemployee " + employee);
+        System.out.println("\nemployee : " + employee.toString());
+
+        // Creating another Employee object with same data
+        Employee employee2 = new Employee(101, "Neha", 150000.00f);
+
+        // Printing the second employee object
+        System.out.println("\nemployee " + employee2);
+        System.out.println("\nemployee : " + employee2.toString());
+
+        // Comparing object references (commented out, would return false since different objects)
+        // System.out.println(employee == employee2);
+
+        // Comparing object content using equals() (must be overridden in Employee class)
+        System.out.println("\n employee.equals(employee2) :" + employee.equals(employee2)); // should be true if equals() is overridden
+
+        // Demonstrating garbage collection reference changes
+        Employee employee4 = employee;  // employee4 points to original employee
+        employee4 = employee2;          // now employee4 points to employee2
+        employee = employee2;           // original employee now also points to employee2
+
+        // Suggesting garbage collection (finalize() will be called if overridden)
+        System.gc();
+
+        // Cloning employee object (clone() must be implemented properly in Employee class)
+        Employee employee3 = employee.clone();
+        System.out.println("Clone of employee : employee3 : " + employee3);
     }
 }
