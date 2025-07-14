bc58dfb900fffee87eff600a9e1e1d5fad8b60d1
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-14 13:01:29 +0530

    Enhance ThrowsDemo class with improved exception handling and division logic; update .gitignore to include additional directories.

--- JavaTrainingPune/src/exception/ThrowsDemo.java
+++ JavaTrainingPune/src/exception/ThrowsDemo.java
@@ -1,9 +1,49 @@
+// Package declaration, helps organize classes
 package exception;
 
+import java.io.IOException;
+
+// Class definition
 public class ThrowsDemo {
-	void division(int num1, int num2) throws ArithmeticException
-	{
-		System.out.println("Division Result of "+num1+ " and "+ num2 + " is :  "+ (num1/num2));
-		System.out.println("End of division !!! ");
-	}
+
+    // Method to perform division, declares that it may throw an ArithmeticException and IOException
+    void division(int num1, int num2) throws ArithmeticException, IOException {
+        // Attempt to divide num1 by num2 and print the result
+        // This may throw ArithmeticException if num2 is zero
+    	
+    	try {
+    	
+        System.out.println("Division Result of " + num1 + " and " + num2 + " is : " + (num1 / num2));
+    	}
+    	catch(ArithmeticException exception)
+    	{
+    		System.out.println("Exception in the Division method : "+ exception.getMessage());
+    	}
+        
+        // This message will be printed only if no exception occurs
+        System.out.println("End of division !!! ");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) throws ArithmeticException,IOException {
+        // Create an instance of ThrowsDemo
+        ThrowsDemo demo = new ThrowsDemo();
+
+        // Try to perform division and handle exceptions
+        try {
+            // Call the division method with 10 and 0
+            demo.division(10, 0);
+        } 
+        // Catch block for ArithmeticException
+        catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        } 
+        // Catch block for IOException
+        catch (IOException e) {
+            System.out.println("IO Error occurred: " + e.getMessage());
+        }
+
+        // This line will always execute (unless a fatal error happens)
+        System.out.println("Program completed.");
+    }
 }
