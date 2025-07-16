50fc0eda87f345c3c9e7a8a08d2d02ca938b4e1e
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-16 13:10:43 +0530

    Update message.text content and enhance UserInputDemo to write user input to the file

--- JavaTrainingPune/src/input_output/UserInputDemo.java
+++ JavaTrainingPune/src/input_output/UserInputDemo.java
@@ -1,11 +1,14 @@
 package input_output;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class UserInputDemo {
     public static void main(String[] args) {
+    	FileOutputStream fileOutputStream=null;
+    	
         try {
             // Create an InputStreamReader to read bytes from the console and decode them into characters
             InputStreamReader inputStreamReader = new InputStreamReader(System.in);
@@ -21,9 +24,20 @@ public class UserInputDemo {
 
             // Display the entered message
             System.out.println("Message is : " + msg);
+            fileOutputStream=new FileOutputStream("D:\\Java IDE\\JavaTrainingPune\\src\\File\\message.text", true);
+            fileOutputStream.write(msg.getBytes());
+            System.out.println("Writing the massage in the file (progress done)......... ");
         } catch (IOException e) {
             // Print the stack trace if an I/O error occurs
             e.printStackTrace();
+        }
+        finally {
+        try {
+        	if(fileOutputStream != null)fileOutputStream.close();
+        }
+        catch(IOException exception) {
+        	exception.printStackTrace();
+        }
         }
     }
 }
