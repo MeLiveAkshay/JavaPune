7da7c56bb0f937f3089852cbad4f42e4f2faf63d
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-16 12:22:00 +0530

    Implement file writing functionality in FileWritingDemo and add message.text with initial content

--- JavaTrainingPune/src/input_output/FileWritingDemo.java
+++ JavaTrainingPune/src/input_output/FileWritingDemo.java
@@ -1,5 +1,32 @@
 package input_output;
 
-public class FileWritingDemo {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class FileWritingDemo {
+	public static void main(String[] args) {
+		FileOutputStream fileOutputStream=null;
+		try {
+			fileOutputStream=new FileOutputStream("D:\\Java IDE\\JavaTrainingPune\\src\\File\\message.text");
+			String msg="Hello !!! This is file writing Demo !!!!";
+			fileOutputStream.write(msg.getBytes());
+			System.out.println("Writing Message into file");
+			
+		}
+		catch(FileNotFoundException exception)
+		{
+			exception.printStackTrace();
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
+		finally {
+			try {
+				if(fileOutputStream != null) fileOutputStream.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 }
